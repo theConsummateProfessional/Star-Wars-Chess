6c098c26d12666a6aeb5bbaac0c4fe2c441af495
theConsummateProfessional <ehood2@una.edu>
2019-12-23 00:13:38 -0600

    Changed comment at top and caption

--- menu.py
+++ menu.py
@@ -1,10 +1,9 @@
-# import numpy as np 
-# import cv2
+#Issue to work on: get the menu to actually work now
 from moviepy.editor import *
 import pygame
 
 def initMenu():
-	pygame.display.set_caption('Pls work')
+	pygame.display.set_caption('Star Wars Chess')
 
 	clip = VideoFileClip('Opening-Crawl.mp4')
 
